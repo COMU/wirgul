f4803720c8cb389b914f3941c4e6ac9c93c54d19
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2012-06-12 10:38:54 +0300

    varsayılan sayfa için view adı değişti

--- web/views.py
+++ web/views.py
@@ -1,11 +1,10 @@
 # Create your views here.
 
-from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 
-def test(request):
+def main(request):
 
     context = {'message': "ok"}
     return render_to_response("test/test.html",
