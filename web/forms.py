a8da35e2ba3b42099b2020661611aebe6e10930e
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2012-06-13 21:58:16 +0300

    bölüm ve fakülte eklendi

---
+++ web/forms.py
@@ -0,0 +1,10 @@
+from django.forms import ModelForm
+from web.models import FirstTimeUser
+from web.models import Faculty
+
+class FirstTimeUserForm(ModelForm):
+    class Meta:
+        model = FirstTimeUser
+        fields = ('name', 'middle_name','surname','email', 'faculty', 'department') #TODO: Form html sayfasında tıklayınca fakulteye ait bolumler gelmeli
+
+
