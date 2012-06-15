1ba473d47ef5f288671dc18df4c4551c5ae1c94a
Ebru Akagündüz <ebru.akagunduz@gmail.com>
2012-06-15 11:01:52 +0300

    Kullanıcı eklemesi saglandı

--- web/forms.py
+++ web/forms.py
@@ -1,11 +1,9 @@
 from django.forms import ModelForm
 from web.models import FirstTimeUser
-from web.models import Faculty
+from captcha.fields import CaptchaField
 
 class FirstTimeUserForm(ModelForm):
+    captcha = CaptchaField()
     class Meta:
         model = FirstTimeUser
-        fields = ('name', 'middle_name','surname','email', 'faculty', 'department')
-
-
-
+        fields = ('name', 'middle_name','surname','email','faculty', 'department')
