619249909d81f7a0d31ea4b3722c7bcb7817d0d1
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-06-26 16:29:17 +0300

    ldap a kayıt almak için fonksiyon eklendi

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,35 @@
 #! -*- coding: utf-8 -*-
-from django.conf import settings
-
-import smtplib
+import ldap
 import string
 from random import choice
+import ldap.modlist as modlist
+from web.models import FirstTimeUser
 from django.core.mail import EmailMultiAlternatives
+
+def generate_passwd():
+    url_id = ''.join([choice(string.letters + string.digits) for i in range(5)])
+    return url_id
+
+def ldap_add_new_user(request):
+    l = ldap.open("127.0.0.1")
+    l.protocol_version = ldap.VERSION3
+    username = "cn=admin, dc=comu,dc=edu,dc=tr"
+    password  = "ldap123"
+    l.simple_bind_s(username, password)
+    dn="mail=melike@comu.edu.tr,ou=personel,ou=people,dc=comu,dc=edu,dc=tr"
+    attrs = {}
+    attrs['objectclass'] = ['organizationalPerson','person','inetOrgPerson']
+    f = FirstTimeUser.objects.all()
+    length = f.count() - 1
+    array_obj = FirstTimeUser.objects.values_list('name','surname','email')
+    attrs['givenName'] = str(array_obj[length][0])
+    attrs['sn'] = str(array_obj[length][1])
+    attrs['cn'] = str(array_obj[length][0]) + str(array_obj[length][1])
+    attrs['mail'] = 'melike@comu.edu.tr'
+    attrs['userPassword'] = generate_passwd()
+    ldif = modlist.addModlist(attrs)
+    l.add_s(dn,ldif)
+    l.unbind_s()
 
 
 def send_email(html_content,to):
@@ -12,9 +37,9 @@ def send_email(html_content,to):
     text_content = 'mesaj icerigi'
     msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
     msg.attach_alternative(html_content, "text/html")
+    print "mail gonderildi"
     msg.send()
 
 def generate_url_id():
     url_id = ''.join([choice(string.letters + string.digits) for i in range(20)])
-    #return HttpResponse(url_id)
     return url_id
