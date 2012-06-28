390b3383fc2d40b2e06719673fd2753ccfcbbacd
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-06-28 12:11:28 +0300

    hata mesajlarını için olan kod değiştirildi

--- web/models.py
+++ web/models.py
@@ -22,10 +22,7 @@ class FirstTimeUserSecret(models.Model):
     passwd_change_time = models.DateTimeField()
 
 class FirstTimeUser(models.Model):
-    def name_field_error(value):
-        if value == "":
-            raise ValidationError('İsminizi Buraya Yazmanız zorunludur')
-    name = models.CharField(max_length=50,validators=[name_field_error])
+    name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50, null=True, blank=True)
     surname = models.CharField(max_length=100)
     faculty = models.ForeignKey(Faculty)
@@ -34,4 +31,12 @@ class FirstTimeUser(models.Model):
     application = models.DateTimeField(auto_now=True)
     url = models.ForeignKey(UrlId, blank=True, null=True)
     secret = models.ForeignKey(FirstTimeUserSecret, blank=True,null=True)
+    def clean(self):
+        if self.email == '':
+            raise ValidationError('Draft entries may not have a publication date.')
+
+
+
+
+
 
