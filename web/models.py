024e24c7c409cbb6e5e9e6aceea1d175ba3b9344
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-06-28 16:33:32 +0300

    etiketlediklerdeki isimler değiştirildi

--- web/models.py
+++ web/models.py
@@ -1,6 +1,5 @@
 #! -*- coding: utf-8 -*-
 from django.db import models
-from django.core.exceptions import ValidationError
 
 class Faculty(models.Model):
     name = models.CharField(max_length=150)
@@ -22,21 +21,14 @@ class FirstTimeUserSecret(models.Model):
     passwd_change_time = models.DateTimeField()
 
 class FirstTimeUser(models.Model):
-    name = models.CharField(max_length=50)
-    middle_name = models.CharField(max_length=50, null=True, blank=True)
-    surname = models.CharField(max_length=100)
-    faculty = models.ForeignKey(Faculty)
-    department = models.ForeignKey(Department)
-    email = models.EmailField(unique=True)
+    name = models.CharField(verbose_name='İsminiz:',max_length=50)
+    middle_name = models.CharField(verbose_name='Orta Adiniz:',max_length=50, null=True, blank=True)
+    surname = models.CharField(verbose_name='Soyadiniz:',max_length=100)
+    faculty = models.ForeignKey(Faculty,verbose_name='Fakülteniz')
+    department = models.ForeignKey(Department,verbose_name='Bölümünüz')
+    email = models.EmailField(unique=True,verbose_name='Mail adresiniz')
     application = models.DateTimeField(auto_now=True)
     url = models.ForeignKey(UrlId, blank=True, null=True)
     secret = models.ForeignKey(FirstTimeUserSecret, blank=True,null=True)
-    def clean(self):
-        if self.email == '':
-            raise ValidationError('Draft entries may not have a publication date.')
-
-
-
-
 
 
