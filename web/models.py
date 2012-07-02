167c6ebb8022cbbc6e0129047e41cb487da630e3
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-07-03 01:30:09 +0300

    email hesabının giriş yaparken tekil mi diye kontrol etme özelliği kaldırıldı

--- web/models.py
+++ web/models.py
@@ -1,5 +1,7 @@
 #! -*- coding: utf-8 -*-
 from django.db import models
+from datetime import datetime
+from django.utils.translation import gettext
 
 class Faculty(models.Model):
     name = models.CharField(max_length=150)
@@ -18,17 +20,20 @@ class UrlId(models.Model):
     url_id = models.CharField(max_length=100)
 
 class FirstTimeUserSecret(models.Model):
-    passwd_change_time = models.DateTimeField()
+    passwd_change_time = models.DateTimeField(auto_now=True)
+
 
 class FirstTimeUser(models.Model):
-    name = models.CharField(verbose_name='İsminiz:',max_length=50)
-    middle_name = models.CharField(verbose_name='Orta Adiniz:',max_length=50, null=True, blank=True)
-    surname = models.CharField(verbose_name='Soyadiniz:',max_length=100)
-    faculty = models.ForeignKey(Faculty,verbose_name='Fakülteniz')
-    department = models.ForeignKey(Department,verbose_name='Bölümünüz')
-    email = models.EmailField(unique=True,verbose_name='Mail adresiniz')
+    name = models.CharField(verbose_name=gettext('İsminiz:'),max_length=50)
+    middle_name = models.CharField(verbose_name=gettext('Orta Adiniz:'),max_length=50, null=True, blank=True)
+    surname = models.CharField(verbose_name=gettext('Soyadiniz:'),max_length=100)
+    faculty = models.ForeignKey(Faculty,verbose_name=gettext('Fakülteniz'))
+    department = models.ForeignKey(Department,verbose_name=gettext('Bölümünüz'))
+    email = models.EmailField(verbose_name=gettext('Mail adresiniz'))
     application = models.DateTimeField(auto_now=True)
     url = models.ForeignKey(UrlId, blank=True, null=True)
     secret = models.ForeignKey(FirstTimeUserSecret, blank=True,null=True)
+
+
 
 
