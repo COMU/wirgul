782e3c1fbf078cd250ca6cc0c60435d0e0536b12
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-07-07 22:23:45 +0300

    ldap'ta zaten kayıtlı olan kişi kullanılan fonksiyon düzenlendi

--- utils/utils.py
+++ utils/utils.py
@@ -24,16 +24,6 @@ def send_email_confirm(to,url_,url_id):
     msg.attach_alternative(html_content, "text/html")
     msg.send()
 
-def generate_passwd():
-    url_id = ''.join([choice(string.letters + string.digits) for i in range(5)])
-    return url_id
-
-def upper_function(string):
-    rep = [ ('İ','I'), ('Ğ','G'),('Ü','U'), ('Ş','S'), ('Ö','O'),('Ç','C'),('ı','i'),('ğ','g'),('ü','u'),('ş','s'),('ö','o'),('ç','c'),(' ','_')]
-    for i, i_replace in rep:
-        string = string.replace(i, i_replace)
-    return string.upper()
-
 def add_new_user(url,passwd):
     obj = LdapHandler()
     obj.connect()
@@ -45,20 +35,25 @@ def add_new_user(url,passwd):
     surname=str(f.surname)
     email = str(f.email)
     if obj.search(email) == 1:
-        sendmail_already_exist(email,url)
-        return
+        send_email_already_exist(email,u)
+        obj.unbind()
+        return 1
     obj.add(name,middle_name,surname,email,passwd)
     obj.unbind()
     send_email_info(url,passwd,email)
+    return 2
 
-def sendmail_already_exist(to,url):   # ldap'ta var ama mysql'de kayıtlı degilse
+def send_email_already_exist(to,url):   # ldap'ta var ama mysql'de kayıtlı degilse
     subject = 'Kullanici Kaydi'
     text_content = 'mesaj icerigi'
     f = FirstTimeUser.objects.get(url=url)
-    name = " ".join([f.name,f.middle_name,f.surname])
-    html_content = '<html><head>'+"Sayin "+name+" "+" sistemimizde zaten kayıtlısınız."+'<p>'+"Parolanızı unuttuysanız"
+    name = str(f.name)
+    middle_name = str(f.middle_name)
+    surname = str(f.surname)
+    name = " ".join([name,middle_name,surname])
+    html_content = '<html><head>'+"Sayin "+name+" "+" sistemimizde zaten kayıtlısınız."+'<br />'+"Parolanızı unuttuysanız"
     html_content +=" sitemizdeki diğer menülerden yararlanabilirsiniz"
-    html_content += '<br />'+settings.MAIL_FOOTER+'</head></html>'
+    html_content += '<br /><br />'+settings.MAIL_FOOTER+'</head></html>'
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER, [to])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
@@ -116,4 +111,12 @@ def generate_url_id():
     url_id = ''.join([choice(string.letters + string.digits) for i in range(20)])
     return url_id
 
+def generate_passwd():
+    url_id = ''.join([choice(string.letters + string.digits) for i in range(5)])
+    return url_id
 
+def upper_function(string):
+    rep = [ ('İ','I'), ('Ğ','G'),('Ü','U'), ('Ş','S'), ('Ö','O'),('Ç','C'),('ı','i'),('ğ','g'),('ü','u'),('ş','s'),('ö','o'),('ç','c'),(' ','_')]
+    for i, i_replace in rep:
+        string = string.replace(i, i_replace)
+    return string.upper()
