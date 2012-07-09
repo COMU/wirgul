192ac8057815b7e48a1aa4f1b7b99a5e2a636e5a
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-07-09 11:16:43 +0300

    mail içerikleri başka bir dosyaya yazıldı

---
+++ utils/mail_content.py
@@ -0,0 +1,24 @@
+#! -*- coding: utf-8 -*-
+SN = "Sayin"
+GUEST_USER_INVALID_REQUEST = "girdiginiz bilgilerden biri veya birkaci hatalidir.Bu yuzden isteginiz yerine getirilemedi\n\n\n"
+
+HOST_USER_CONFIRM = "adli kullanici sizin konugunuz oldugunu belirterek sistemimize kayit yaptirmak " \
+                    "istiyor.Eger bu kisiyi taniyorsaniz onaylamak icin asagidaki linke"
+
+CLICK = "tiklayiniz."
+
+GUEST_USER_CONFIRM = "misafir olarak geldiginiz kisiye mail gonderilmistir. " \
+                      "Mail onaylandiktan sonra kullanici adi ve parolaniz size mail olarak gonderilecektir \n\n\n"
+
+CHANGE_PASSWORD_CONFIRM = "parola degisim isleminizi onaylamak icin linke "
+
+CHANGE_PASSWORD_INFO = "yeni parolaniz : "
+
+NEW_USER_INFO = "kullanici adi ve parola bilgilerinizi alabilmek icin asagidaki linke"
+
+USER_ALREADY_EXİST = "sistemimizde zaten kayitlisiniz.Parolanizi unuttuysaniz sitemizdeki diger menulerden yararlanabilirsiniz"
+
+PASSWORD = " parolaniz: "
+
+USER_NAME = "kullanici adiniz : "
+
