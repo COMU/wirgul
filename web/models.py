84cc1cd8124b929e376b43603d9e6a3f3af514ac
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-07-09 09:17:57 +0300

    misafir kullanıcı için veritabanında olan alanlar düzenlendi

--- web/models.py
+++ web/models.py
@@ -43,13 +43,14 @@ class GuestUser(models.Model):
     surname = models.CharField(verbose_name=gettext('Soyadiniz:'),max_length=100)
     guest_user_email = models.EmailField(verbose_name=gettext('Mail Adresiniz'))
     email = models.EmailField(verbose_name=gettext('Misafiri Oldugunuz Kişinin mail adresi:'))
+    url = models.CharField(max_length=100)
     TIME_CHOICES = (
         (1, 'SAAT'),
         (2, 'GUN'),
         (3, 'HAFTA'),
         )
-    type = models.CharField(choices=TIME_CHOICES,default=1,max_length=10,verbose_name=gettext("Saat Gün ya da Hafta Seçiniz"))
-    time_duration = models.CharField(choices=settings.TIME_DURATION_CHOICES,verbose_name="Süreyi Seçiniz",max_length=10)
+    type = models.CharField(blank=True,null=True,choices=TIME_CHOICES,default=1,max_length=10,verbose_name=gettext("Saat Gün ya da Hafta Seçiniz"))
+    time_duration = models.CharField(blank=True,null=True,choices=settings.TIME_DURATION_CHOICES,verbose_name="Süreyi Seçiniz",max_length=10)
     start = models.DateTimeField(auto_now=True)
     guest_user_phone = models.IntegerField("Telefon Numaranız :",max_length=10)
 
