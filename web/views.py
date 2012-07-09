6f58eb088fe9963c153a42843b9fd842c72d3d69
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-07-09 09:11:54 +0300

    misafir kullanıcı için fonksiyonlar eklendi

--- web/views.py
+++ web/views.py
@@ -1,7 +1,8 @@
 #! -*- coding: utf-8 -*-
-from utils.utils import generate_url_id,generate_passwd,add_new_user,LdapHandler
+from utils.utils import generate_url_id,generate_passwd,add_new_user,LdapHandler,user_already_exist
 from utils.utils import new_user_confirm,upper_function,change_password_confirm,change_password_info
 from django.http import HttpResponse
+from utils.utils import guest_user_confirm,guest_user_invalid_request,host_user_confirm
 from web.forms import FirstTimeUserForm,FirstTimeUser,PasswordChangeForm,GuestUserForm,GuestUser
 from web.models import Faculty,Department,UrlId
 from django.template.context import RequestContext
@@ -78,7 +79,7 @@ def new_user(request):
                 new_user_confirm(email,url_,urlid_obj)
                 context['form'] = form
                 context['web']  = "new_user"
-                return render_to_response("new_user/send_mail.html",
+                return render_to_response("new_user/new_user_confirm.html",
                     context_instance=RequestContext(request, context))
             else:
                 context['form'] = form
@@ -123,9 +124,45 @@ def guest_user(request):
            surname = request.POST['surname']
            guest_user_email = request.POST['guest_user_email']
            email = request.POST['email']
+           guest_user_phone = request.POST['guest_user_phone']
            surname = upper_function(str(surname))
            name=upper_function(str(name))
            middle_name = upper_function(str(middle_name))
+           obj = LdapHandler()
+           obj.connect()
+           obj.bind()
+           if obj.search(guest_user_email) == 1:  # eger kayıt olmak isteyen misafir zaten ldap'ta kayıtllıysa
+               obj.unbind()
+               user_already_exist(guest_user_email)
+               context['form'] = form
+               context['web']  = "guest_user"
+               context['info'] = 'Misafir Kullanıcı Kaydı'
+               return render_to_response("guest_user/guest_user_confirm.html",
+                   context_instance=RequestContext(request, context))
+           elif obj.search(email) != 1:   # eger misafiri olunmak istenen kişi yoksa
+               obj.unbind()
+               guest_user_invalid_request(email)
+               context['form'] = form
+               context['web']  = "guest_user"
+               context['info'] = 'Misafir Kullanıcı Kaydı'
+               return render_to_response("guest_user/guest_user_confirm.html",
+                   context_instance=RequestContext(request, context))
+           url = generate_url_id()
+           guest_user_obj, created = GuestUser.objects.get_or_create(name=name,middle_name=middle_name,
+               surname=surname,email=email,guest_user_email=guest_user_email,url=url,guest_user_phone=guest_user_phone)
+           print "--"
+           guest_user_confirm(guest_user_email) # misafir kullanıcıya ev sahibi kullanıcıya mail atıldıgının bildirilmesi
+           print "wdww"
+           if email.find("@comu.edu.tr") != -1:
+               mail_adr = email.split("@")
+               email = mail_adr[0]
+               email = "".join([mail_adr[0],"@gmail.com"])
+           host_user_confirm(email,guest_user_email)
+           context['form'] = form
+           context['web']  = "guest_user"
+           context['info'] = 'Misafir Kullanıcı Kaydı'
+           return render_to_response("guest_user/guest_user_confirm.html",
+                   context_instance=RequestContext(request, context))
         else:
             context['form'] = form
             context['web']  = "guest_user"
@@ -138,6 +175,25 @@ def guest_user(request):
         context['info'] = 'Misafir Kullanıcı Kaydı'
         return render_to_response("guest_user/guest_user.html",
             context_instance=RequestContext(request, context))
+
+def guest_user_registration(request,url_id):
+    g = GuestUser.objects.get(url = url_id)
+    email = str(g.emai)
+    name = str(g.name)
+    middle_name = str(g.middle_name)
+    surname = str(g.surname)
+    password = generate_passwd()
+    context = dict()
+    context['url_id'] = url_id
+    obj = LdapHandler()
+    obj.connect()
+    obj.bind()
+    obj.add(name,middle_name,surname,email,password)
+    obj.unbind()
+    return render_to_response("guest_user/guest_user_info.html",
+            context_instance=RequestContext(request, context))
+
+
 
 def password_change_registration(request,url_id):
     context = dict()
@@ -163,6 +219,6 @@ def new_user_registration(request,url_id):
         return render_to_response("new_user/new_user_info.html",
             context_instance=RequestContext(request, context))
     # ldap'ta zaten kayıtlıysa gosterilen sayfa
-    return render_to_response("new_user/send_mail.html",
+    return render_to_response("new_user/new_user_confirm.html",
         context_instance=RequestContext(request, context))
     
