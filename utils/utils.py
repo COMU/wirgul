816ed28a63d862063e37036c50385e6695c4f190
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-07-09 14:47:28 +0300

    ldap'taki fonksiyonlara returnler eklendiği için utils.py deki fonksiyonlarada birkaç kontrol eklendi

--- utils/utils.py
+++ utils/utils.py
@@ -9,6 +9,7 @@ from ldapmanager import *
 import mail_content
 from django.core.mail import send_mail
 from django.core.mail import EmailMultiAlternatives
+import status
 
 def guest_user_invalid_request(to):
     subject = 'Misafir Kullanici Bilgilendirme'
@@ -29,7 +30,7 @@ def host_user_confirm(to,guest_user_email):
     path_ = reverse('password_change_registration', kwargs={'url_id': str(g.url)})
     name =  " ".join([f.name,f.middle_name,f.surname])  # ev sahibi kullanıcının adı soyadı
     mail_text = " ".join(['<html><head>',mail_content.SN,name,guest_name,mail_content.HOST_USER_CONFIRM,'<a href="http://'
-        ,settings.SERVER_ADRESS,path_,'>',mail_content.CLICK,'</a><br/><br/>',settings.MAIL_FOOTER,'</head></html>'])
+        ,settings.SERVER_ADRESS,path_,'">',mail_content.CLICK,'</a><br/><br/>',settings.MAIL_FOOTER,'</head></html>'])
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER ,[to])
     msg.attach_alternative(mail_text, "text")
     msg.send()
@@ -53,8 +54,8 @@ def change_password_confirm(to,url_):
     f = FirstTimeUser.objects.get(email= to)
     name =  " ".join([f.name,f.middle_name,f.surname])
     path_ = reverse('password_change_registration', kwargs={'url_id': url_})
-    mail_text = " ".join(['<html><head>',mail_content.SN,name,mail_content.CHANGE_PASSWORD_CONFIRM,'<a href="http://'
-        ,settings.SERVER_ADRESS,path_,'>',mail_content.CLICK],'</a><br/><br/>',settings.MAIL_FOOTER,'</head></html>')
+    link = '<a href="http://'+settings.SERVER_ADRESS+path_+'">'+mail_content.CLICK+'</a><br/><br/>'
+    mail_text = " ".join(['<html><head>',mail_content.SN,name,mail_content.CHANGE_PASSWORD_CONFIRM,link,settings.MAIL_FOOTER,'</head></html>'])
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER ,[to])
     msg.attach_alternative(mail_text, "text/html")
     msg.send()
@@ -65,7 +66,7 @@ def change_password_info(to,password):
     email_obj = FirstTimeUser.objects.get(email= to)
     name = str(email_obj.name+" "+email_obj.middle_name+" "+email_obj.surname)
     mail_text = " ".join(['<html><head>',mail_content.SN,name,
-                          mail_content.CHANGE_PASSWORD_INFO,password,'<br /><br /><br />',settings.MAIL_FOOTER])
+                          mail_content.CHANGE_PASSWORD_INFO,password,'<br /><br /><br />',settings.MAIL_FOOTER,'</head></html>'])
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER, [to])
     msg.attach_alternative(mail_text, "text/html")
     msg.send()
@@ -76,13 +77,14 @@ def new_user_confirm(to,url_,url_id):
     f = FirstTimeUser.objects.get(url=url_id)
     name =  " ".join([f.name,f.middle_name,f.surname])
     path_ = reverse('new_user_registration_view', kwargs={'url_id': url_})
-    mail_text = " ".join(['<html><head>',name,mail_content.NEW_USER_INFO,'<p><a href="http://'
-        ,settings.SERVER_ADRESS,path_,mail_content.CLICK,'</a><br/><br/>',settings.MAIL_FOOTER,'</head></html>'])
+    link = '<a href="http://'+settings.SERVER_ADRESS+path_+'">'+mail_content.CLICK+'</a><br/><br/>'
+    mail_text = " ".join(['<html><head>',name,mail_content.NEW_USER_INFO,link,settings.MAIL_FOOTER,'</head></html>'])
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER ,[to])
     msg.attach_alternative(mail_text, "text/html")
     msg.send()
 
 def add_new_user(url,passwd):
+
     obj = LdapHandler()
     obj.connect()
     obj.bind()
@@ -93,13 +95,16 @@ def add_new_user(url,passwd):
     surname=str(f.surname)
     email = str(f.email)
     if obj.search(email) == 1:
-        user_already_exist(email,u)
+        user_already_exist(email)
         obj.unbind()
-        return False   # ldap'ta kayıt varsa bu kişi kayıtlı cevabını döndürsün diye return 1 yazıldı
-    obj.add(name,middle_name,surname,email,passwd)
-    obj.unbind()
-    new_user_info(url,passwd,email)
-    return True
+        return status.USER_EXIST   # ldap'ta kayıt varsa bu kişi kayıtlı cevabını döndürsün diye return 1 yazıldı
+    if obj.add(name,middle_name,surname,email,passwd):  # ldap'a ekleme yapıldıysa true doner
+        obj.unbind()
+        new_user_info(url,passwd,email)
+        return status.ADD_OK
+    else: # herhangi bir sorun olusup yeni kullanici kaydi alinamadiysa
+        return status.ADD_ERROR
+
 
 def user_already_exist(to):   # ldap'ta var ama mysql'de kayıtlı degilse
     subject = 'Kullanici Kaydi'
@@ -109,7 +114,7 @@ def user_already_exist(to):   # ldap'ta var ama mysql'de kayıtlı degilse
     middle_name = str(f.middle_name)
     surname = str(f.surname)
     name = " ".join([name,middle_name,surname])
-    mail_text = " ".join(['<html><head>',mail_content.SN,name,mail_content.USER_ALREADY_EXİST,'<br /><br />',settings.MAIL_FOOTER,'</head></html>'])
+    mail_text = " ".join(['<html><head>',mail_content.SN,name,mail_content.USER_ALREADY_EXIST_TEXT,'<br /><br />',settings.MAIL_FOOTER,'</head></html>'])
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER, [to])
     msg.attach_alternative(mail_text, "text/html")
     msg.send()
