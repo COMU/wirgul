3b4d1c37aea0be40482f1ee7f42e5d2d26a38947
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-07-12 09:08:18 +0300

    girilen kullanıcı isimlerindeki Türkçe karakter sorunu düzeltildi

--- utils/utils.py
+++ utils/utils.py
@@ -77,14 +77,13 @@ def new_user_confirm(to,url_,url_id):
     f = FirstTimeUser.objects.get(url=url_id)
     name =  " ".join([f.name,f.middle_name,f.surname])
     path_ = reverse('new_user_registration_view', kwargs={'url_id': url_})
-    link = '<a href="http://'+settings.SERVER_ADRESS+path_+'">'+mail_content.CLICK+'</a><br/><br/>'
-    mail_text = " ".join(['<html><head>',name,mail_content.NEW_USER_INFO,link,settings.MAIL_FOOTER,'</head></html>'])
+    link = '<a href="http://'+settings.SERVER_ADRESS+path_+'">'+mail_content.LINK+'</a><br/><br/>'
+    mail_text = " ".join(['<html><head>',mail_content.SN,name,mail_content.NEW_USER_INFO,link,mail_content.CLICK,'<br /><br />',settings.MAIL_FOOTER,'</head></html>'])
     msg = EmailMultiAlternatives(subject, text_content, settings.EMAIL_HOST_USER ,[to])
     msg.attach_alternative(mail_text, "text/html")
     msg.send()
 
 def add_new_user(url,passwd):
-
     obj = LdapHandler()
     obj.connect()
     obj.bind()
@@ -94,10 +93,6 @@ def add_new_user(url,passwd):
     middle_name = str(f.middle_name)
     surname=str(f.surname)
     email = str(f.email)
-    if obj.search(email) == 1:
-        user_already_exist(email)
-        obj.unbind()
-        return status.USER_EXIST   # ldap'ta kayıt varsa bu kişi kayıtlı cevabını döndürsün diye return 1 yazıldı
     if obj.add(name,middle_name,surname,email,passwd):  # ldap'a ekleme yapıldıysa true doner
         obj.unbind()
         new_user_info(url,passwd,email)
@@ -122,7 +117,6 @@ def user_already_exist(to):   # ldap'ta var ama mysql'de kayıtlı degilse
 def new_user_info(url,passwd,to):
     subject = 'Kullanici Kaydi'
     text_content = 'mesaj icerigi'
-    path_ = reverse('new_user_registration_view', kwargs={'url_id':url})
     u = UrlId.objects.get(url_id=url)
     f = FirstTimeUser.objects.get(url=u)
     name = str(f.name)
@@ -148,8 +142,8 @@ def generate_passwd():
     url_id = ''.join([choice(string.letters + string.digits) for i in range(5)])
     return url_id
 
-def upper_function(string):
-    rep = [ ('İ','I'), ('Ğ','G'),('Ü','U'), ('Ş','S'), ('Ö','O'),('Ç','C'),('ı','i'),('ğ','g'),('ü','u'),('ş','s'),('ö','o'),('ç','c'),(' ','_')]
+def upper_function(s):
+    rep = [ (u'İ',u'I'), (u'Ğ',u'G'),(u'Ü',u'U'), (u'Ş',u'S'), (u'Ö',u'O'),(u'Ç',u'C'),(u'ı',u'i'),(u'ğ',u'g'),(u'ü',u'u'),(u'ş',u's'),(u'ö',u'o'),(u'ç',u'c')]
     for i, i_replace in rep:
-        string = string.replace(i, i_replace)
-    return string.upper()
+        s = s.replace(i, i_replace)
+    return s.upper()
