43a89dff40c460374074584d9bed922f6df58a87
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-07-26 11:29:01 +0300

    form mesajları için bir dosya oluşturuldu

---
+++ utils/messages.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+
+REQUIRED_FORM_MESSAGE = "Bu alanı doldurmak zorundasınız"
+INVALID_EMAIL_FORM_MESSAGE = "Geçersiz bir eposta adresi girdiniz"
+INVALID_CAPTCHA_FORM_MESSAGE = "Gördüğünüz harfleri yanlış girdiniz"
