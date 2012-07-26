25ae4389f572c4065a20e4f4b2961ecf6107f3cc
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-07-26 12:53:30 +0300

    model dosyasındaki mesajlar sabitlere atandı

--- utils/messages.py
+++ utils/messages.py
@@ -4,3 +4,10 @@
 REQUIRED_FORM_MESSAGE = "Bu alanı doldurmak zorundasınız"
 INVALID_EMAIL_FORM_MESSAGE = "Geçersiz bir eposta adresi girdiniz"
 INVALID_CAPTCHA_FORM_MESSAGE = "Gördüğünüz harfleri yanlış girdiniz"
+
+WEB_MODEL_YOURNAME = "İsminiz"
+WEB_MODEL_MIDDLE_NAME = "Orta İsminiz"
+WEB_MODEL_SURNAME = "Soyisminiz"
+WEB_MODEL_EMAIL = "Eposta Adresiniz"
+WEB_MODEL_FACULTY = "Fakülteniz"
+WEB_MODEL_DEPARTMENT = "Bölümünüz"
