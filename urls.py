f8e7a9cc7436f6adf1aa8e5c5fc139a5634841cc
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-07-26 11:12:01 +0300

    kök dizinindeki url için adres uzayı sadeleştirildi

--- urls.py
+++ urls.py
@@ -8,32 +8,9 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'web.views.main', name='main_view'),
-    url(r'^new/$', 'web.views.new_user', name='new_user_view'),
-    url(r'^already_exist/$', 'web.views.new_user', name='new_user_already_exist_view'),
-    url(r'^password_change/invalid_mail/$', 'web.views.password_change', name='invalid_mail_views'),
-    url(r'^password_change/$', 'web.views.password_change', name='password_change_views'),
-    url(r'^get_times/$', 'web.views.get_times', name='get_times'),
-    url(r'^guest_user/$', 'web.views.guest_user', name='guest_user_view'),
-    url(r'^doesnt_exist/$', 'web.views.new_user', name='doesnt_exist_view'),
-    url(r'^new/new_user_confirm/$', 'web.views.new_user', name='new_user_confirm'),
-    url(r'^new/(?P<url_id>\w+)/$', 'web.views.new_user_registration', name='new_user_registration_view'),
-    url(r'^get_departments/$', 'web.views.get_departments', name='get_departments'),
     (r'^i18n/', include('django.conf.urls.i18n')),
     url(r'^captcha/', include('captcha.urls')),
-    url(r'^password_change/(?P<url_id>\w+)/$', 'web.views.password_change_registration', name='password_change_registration'),
-    url(r'^guest_user/$', 'web.views.guest_user', name='guest_user_view'),
-    url(r'^guest_user/(?P<url_id>\w+)/$', 'web.views.guest_user_registration', name='guest_user_registration_view'),
-    #url(r'^new/$', 'web.views.faculty', name='faculty'),
-    #url(r'^new/$', 'web.views.faculty', name='faculty'),
-    # url(r'^$', 'wirgul.views.home', name='home'),
-    # url(r'^wirgul/', include('wirgul.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    (r'^/', include('wirgul.web.urls')),
 )
 
 if settings.DEBUG:
