56ab8e4690a5aa824a76019aa361889ac721b86c
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-07-27 16:37:04 +0300

    yeni kullanıcı için eposta gönderimindeki mesajlar sabitlere atandı, önceki tanımlanmış mesajlar kaldırıldı

--- utils/mail_content.py
+++ utils/mail_content.py
@@ -1,25 +1,47 @@
 #! -*- coding: utf-8 -*-
-SN = u"Sayın"
-GUEST_USER_INVALID_REQUEST = u"girdiğiniz bilgilerden biri veya birkaçi hatalidir.Bu yüzden isteğiniz yerine getirilemedi\r\n"
+NEW_USER_APPLICATION_SUBJECT = "Yeni Kullanıcı Başvurusu"
+DEAR = u"Sayın "
+NEW_USER_APPLICATION_TEXT_BODY = u"Üniversitemizdeki kablosuz ağdan yararlanmak için yeni kullanıcı başvurusunda bulundunuz.\r\n \
+                                 Başvurunuzun aktif olması için lütfen aşağıdaki bağlantıya tıklayınız.\r\n"
+NEW_USER_LINK_TEXT = u"Yeni Kullanıcı Etkinleştirme Bağlantısı"
 
-HOST_USER_CONFIRM = u"adlı kullanıcı sizin konuğunuz olduğunu belirterek sistemimize kayıt yaptırmak istiyor." \
-                    u"Eğer bu kişiyi tanıyorsanız onaylamak için aşağıdaki linke"
+NEW_USER_HTML_BODY = u"""\
+    <html>
+      <head>
+	  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      </head>
+      <body>
+      """
+NEW_USER_HTML_DEAR_STARTS = u"<p>Sayın "
+NEW_USER_HTML_DEAR_ENDS = u",</p><br />"
+NEW_USER_HTML_BODY = u"<p>Üniversitemizdeki kablosuz ağdan yararlanmak için yeni kullanıcı başvurusunda bulundunuz.<br /> \
+                        Başvurunuzun aktif olması için lütfen aşağıdaki bağlantıya tıklayınız.<br /></p>"
 
-CLICK = u"tıklayınız."
 
-GUEST_USER_CONFIRM = u"misafir olarak geldiğiniz kişiye mail gönderilmiştir." \
-                     u"Mail onaylandıktan sonra kullanıcı adı ve parolanız size mail olarak gönderilecektir \r\n"
-CHANGE_PASSWORD_CONFIRM = u"parola değişim işleminizi onaylamak için linke "
 
-CHANGE_PASSWORD_INFO = u"yeni parolanız : "
 
-NEW_USER_INFO = u"kullanıcı adı ve parola bilgilerinizi alabilmek için"
-LINK = u"linke"
 
-USER_ALREADY_EXIST_TEXT = u"sistemimizde zaten kayıtlısınız.Parolanızı unuttuysanız sitemizdeki diğer menülerden yararlanabilirsiniz"
 
-PASSWORD = u" parolanız: "
-
-USER_NAME = u"kullanıcı adınız : "
+#GUEST_USER_INVALID_REQUEST = u"girdiğiniz bilgilerden biri veya birkaçi hatalidir.Bu yüzden isteğiniz yerine getirilemedi\r\n"
+#
+#HOST_USER_CONFIRM = u"adlı kullanıcı sizin konuğunuz olduğunu belirterek sistemimize kayıt yaptırmak istiyor." \
+#                    u"Eğer bu kişiyi tanıyorsanız onaylamak için aşağıdaki linke"
+#
+#CLICK = u"tıklayınız."
+#
+#GUEST_USER_CONFIRM = u"misafir olarak geldiğiniz kişiye mail gönderilmiştir." \
+#                     u"Mail onaylandıktan sonra kullanıcı adı ve parolanız size mail olarak gönderilecektir \r\n"
+#CHANGE_PASSWORD_CONFIRM = u"parola değişim işleminizi onaylamak için linke "
+#
+#CHANGE_PASSWORD_INFO = u"yeni parolanız : "
+#
+#NEW_USER_INFO = u"kullanıcı adı ve parola bilgilerinizi alabilmek için"
+#LINK = u"linke"
+#
+#USER_ALREADY_EXIST_TEXT = u"sistemimizde zaten kayıtlısınız.Parolanızı unuttuysanız sitemizdeki diğer menülerden yararlanabilirsiniz"
+#
+#PASSWORD = u" parolanız: "
+#
+#USER_NAME = u"kullanıcı adınız : "
 
 
