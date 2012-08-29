1bd8f17f334fd55379dd10134558c92a92a6d143
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-08-29 08:53:28 +0300

    Url sınıfı altına url in oluştuğu zaman ve tıklanma durumu toplandı. Diğer sınıflarda yabancı anahtar tanımlandı

--- web/models.py
+++ web/models.py
@@ -3,7 +3,9 @@ from django.db import models
 from django.utils.translation import gettext
 from django.conf import settings
 
-from wirgul.utils.messages import WEB_MODEL_EMAIL, WEB_MODEL_MIDDLE_NAME, WEB_MODEL_SURNAME, WEB_MODEL_YOURNAME, WEB_MODEL_FACULTY, WEB_MODEL_DEPARTMENT
+from wirgul.utils.messages import WEB_MODEL_EMAIL, WEB_MODEL_MIDDLE_NAME, WEB_MODEL_SURNAME, WEB_MODEL_YOURNAME, \
+    WEB_MODEL_FACULTY, WEB_MODEL_DEPARTMENT, WEB_MODEL_GUEST_EMAIL, WEB_MODEL_CHOOSE_DURATION_TYPE, \
+    WEB_MODEL_CHOOSE_DURATION, WEB_MODEL_TEL_NUMBER
 
 class Faculty(models.Model):
     name = models.CharField(max_length=150)
@@ -18,17 +20,17 @@ class Department(models.Model):
     def __unicode__(self):
         return u'%s' % (self.name)
 
-class UrlId(models.Model):
+class Url(models.Model):
     url_id = models.CharField(max_length=100)
+    url_create_time = models.DateTimeField(auto_now=True) # urlin ilk oluşturulduğu zaman
+    status = models.BooleanField(default=False) # url e tıklama durumu kontrolü
 
 class FirstTimeUserSecret(models.Model):
     passwd_change_time = models.DateTimeField(auto_now=True)
 
 class PasswordChange(models.Model):
     email = models.EmailField(verbose_name=gettext(WEB_MODEL_EMAIL))
-    url = models.CharField(max_length=100)
-    url_create_time = models.DateTimeField()
-    status = models.BooleanField(default=False)
+    url = models.ForeignKey(Url, blank=True, null=True)
 
 class FirstTimeUser(models.Model):
     name = models.CharField(verbose_name=gettext(WEB_MODEL_YOURNAME),max_length=50)
@@ -37,23 +39,21 @@ class FirstTimeUser(models.Model):
     faculty = models.ForeignKey(Faculty,verbose_name=gettext(WEB_MODEL_FACULTY))
     department = models.ForeignKey(Department,verbose_name=gettext(WEB_MODEL_DEPARTMENT))
     email = models.EmailField(verbose_name=gettext(WEB_MODEL_EMAIL))
-    application = models.DateTimeField(auto_now=True)
-    url = models.ForeignKey(UrlId, blank=True, null=True)
+    url = models.ForeignKey(Url, blank=True, null=True)
     secret = models.ForeignKey(FirstTimeUserSecret, blank=True,null=True)
 
 class GuestUser(models.Model):
-    name = models.CharField(verbose_name=gettext('İsminiz:'),max_length=50)
-    middle_name = models.CharField(verbose_name=gettext('Orta Adiniz:'),max_length=50, null=True, blank=True)
-    surname = models.CharField(verbose_name=gettext('Soyadiniz:'),max_length=100)
-    guest_user_email = models.EmailField(verbose_name=gettext('Mail Adresiniz'))
-    email = models.EmailField(verbose_name=gettext('Misafiri Oldugunuz Kişinin mail adresi:'))
-    url = models.CharField(max_length=100)
+    name = models.CharField(verbose_name=gettext(WEB_MODEL_YOURNAME),max_length=50)
+    middle_name = models.CharField(verbose_name=gettext(WEB_MODEL_MIDDLE_NAME),max_length=50, null=True, blank=True)
+    surname = models.CharField(verbose_name=gettext(WEB_MODEL_SURNAME),max_length=100)
+    guest_user_email = models.EmailField(verbose_name=gettext(WEB_MODEL_EMAIL))
+    email = models.EmailField(verbose_name=gettext(WEB_MODEL_GUEST_EMAIL))
+    guest_user_phone = models.IntegerField(WEB_MODEL_TEL_NUMBER, max_length=10)
+    url = models.ForeignKey(Url, blank=True, null=True)
     TIME_CHOICES = (
         (1, 'SAAT'),
         (2, 'GUN'),
         (3, 'HAFTA'),
         )
-    type = models.CharField(blank=True,null=True,choices=TIME_CHOICES,default=1,max_length=10,verbose_name=gettext("Saat Gün ya da Hafta Seçiniz"))
-    time_duration = models.CharField(blank=True,null=True,choices=settings.TIME_DURATION_CHOICES,verbose_name="Süreyi Seçiniz",max_length=10)
-    start = models.DateTimeField(auto_now=True)
-    guest_user_phone = models.IntegerField("Telefon Numaranız :",max_length=10)
+    type = models.CharField(blank=True,null=True,choices=TIME_CHOICES,default=1,max_length=10,verbose_name=gettext(WEB_MODEL_CHOOSE_DURATION_TYPE))
+    time_duration = models.CharField(blank=True,null=True,choices=settings.TIME_DURATION_CHOICES,verbose_name=WEB_MODEL_CHOOSE_DURATION, max_length=10)
