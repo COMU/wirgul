75a274a927bb5339e5936c4dd4d131e8948d88f2
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-08-30 10:43:52 +0300

    fakülte ve bölümlerin yeni hali eklendi

--- sql/faculty.sql
+++ sql/faculty.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (i486)
+-- MySQL dump 10.13  Distrib 5.5.24, for debian-linux-gnu (i686)
 --
--- Host: localhost    Database: ldapusers_db
+-- Host: localhost    Database: wirguldb
 -- ------------------------------------------------------
--- Server version	5.1.49-3
+-- Server version	5.5.24-0ubuntu0.12.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -19,12 +19,23 @@
 -- Table structure for table `web_faculty`
 --
 
+DROP TABLE IF EXISTS `web_faculty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `web_faculty` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `web_faculty`
 --
 
 LOCK TABLES `web_faculty` WRITE;
 /*!40000 ALTER TABLE `web_faculty` DISABLE KEYS */;
-INSERT INTO `web_faculty` VALUES (1,'Ayvacık Meslek Yüksekokulu'),(2,'Bayramiç Meslek Yüksekokulu'),(3,'Beden Eğitimi ve Spor Yüksekokulu'),(4,'Biga İktisadi ve İdari Bilimler Fakültesi'),(5,'Biga İktisadi ve İdari Bilimler Fakültes'),(6,'Biga Meslek Yüksekokulu'),(7,'Çan Meslek Yüksekokulu'),(8,'Çanakkale Meslek Yüksekokulu'),(9,'Canakkale Sağlık Hizmetleri Meslek Yüksekokulu'),(10,'Canakkale Sağlık Hizmetleri Meslek Yüksekoulu'),(11,'Canakkale Sağlık Yüksekokulu'),(12,'Eğitim Fakültesi'),(13,'Ezine Meslek Yüksekokulu'),(14,'Fen Edebiyat Fakültesi'),(15,'Gelibolu Piri Reis Meslek Yüksekokulu'),(16,'Gökçeada Meslek Yüksekokulu'),(17,'Gökçeada Uygulamalı Bilimler Yüksekokulu'),(18,'Güzel Sanatlar Fakültesi'),(19,'İlahiyat Fakültesi'),(20,'Lapseki Meslek Yüksekokulu'),(21,'Mühendislik ve Mimarlık Fakültesi'),(22,'Su Ürünleri Fakültesi'),(23,'Tıp Fakültesi'),(24,'Turizm İşletmeciliği ve Otelcilik Yüksek'),(25,'Yenice Meslek Yüksekokulu'),(26,'Ziraat Fakültesi'),(27,'Turizm İşletmeciliği ve Otelcilik Yüksekokulu'),(28,'Test Fakültesi'),(29,'Eğitim Bilimleri Enstitüsü'),(30,'Fen Bilimleri Enstitüsü'),(31,'Sosyal Bilimler Enstitüsü'),(32,'Sağlık Bilimleri Enstitüsü'),(33,'11302201012@comu.edu.tr'),(34,'Konferans'),(35,'TEST'),(36,'STAJ');
+INSERT INTO `web_faculty` VALUES (1,'Ayvacık Meslek Yüksekokulu'),(2,'Bayramiç Meslek Yüksekokulu'),(3,'Beden Eğitimi ve Spor Yüksekokulu'),(4,'Biga İktisadi ve İdari Bilimler Fakültesi'),(5,'Biga İktisadi ve İdari Bilimler Fakültes'),(6,'Biga Meslek Yüksekokulu'),(7,'Çan Meslek Yüksekokulu'),(8,'Çanakkale Meslek Yüksekokulu'),(9,'Canakkale Sağlık Hizmetleri Meslek Yüksekokulu'),(10,'Canakkale Sağlık Hizmetleri Meslek Yüksekoulu'),(11,'Canakkale Sağlık Yüksekokulu'),(12,'Eğitim Fakültesi'),(13,'Ezine Meslek Yüksekokulu'),(14,'Fen Edebiyat Fakültesi'),(15,'Gelibolu Piri Reis Meslek Yüksekokulu'),(16,'Gökçeada Meslek Yüksekokulu'),(17,'Gökçeada Uygulamalı Bilimler Yüksekokulu'),(18,'Güzel Sanatlar Fakültesi'),(19,'İlahiyat Fakültesi'),(20,'Lapseki Meslek Yüksekokulu'),(21,'Mühendislik ve Mimarlık Fakültesi'),(22,'Su Ürünleri Fakültesi'),(23,'Tıp Fakültesi'),(24,'Turizm İşletmeciliği ve Otelcilik Yüksek'),(25,'Yenice Meslek Yüksekokulu'),(26,'Ziraat Fakültesi'),(27,'Turizm İşletmeciliği ve Otelcilik Yüksekokulu'),(29,'Eğitim Bilimleri Enstitüsü'),(30,'Fen Bilimleri Enstitüsü'),(31,'Sosyal Bilimler Enstitüsü'),(32,'Sağlık Bilimleri Enstitüsü'),(38,'Rektörlüğe Bağlı Birimler');
 /*!40000 ALTER TABLE `web_faculty` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -37,4 +48,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-06-13 10:00:36
+-- Dump completed on 2012-08-30 10:40:36
