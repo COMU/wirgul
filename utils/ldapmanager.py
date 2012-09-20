0689d5918f35bb135454f7b8d282420769047ba7
Oğuz Yarımtepe <oguzyarimtepe@gmail.com>
2012-09-20 09:08:40 +0300

    misafir kullanıcı için tanımlamalar yapıldı, LDAP ekleme kısmında öğrenci için düzenleme yapıldı, kullanılan alan adları sabitler olarak ayarlandı ve ayar dosyasından çekildi

--- utils/ldapmanager.py
+++ utils/ldapmanager.py
@@ -29,18 +29,27 @@ class LdapHandler:
     def unbind(self):
         self.server.unbind_s()
 
-    def add(self,name,middle_name,surname,email,passwd):
+    def add(self,name,middle_name,surname,email,passwd,guest=False):
         attrs = {}
-        if email.find("@gmail.com") != -1:
+        if not guest:
+            if email.find(settings.EDUROAM_EXCEPTION_DOMAIN) != -1:
+                    mail_adr = email.split("@")
+                    email_prefix = mail_adr[0]
+                    dn= "".join(["mail=",email_prefix,"@",settings.EDUROAM_DOMAIN,"ou=personel,ou=people,dc=comu,dc=edu,dc=tr"])
+                    attrs['mail'] = "".join([email_prefix,"".join(["@", settings.EDUROAM_DOMAIN])])
+            else:
                 mail_adr = email.split("@")
-                email = mail_adr[0]
-                dn= "".join(["mail=",email,"@comu.edu.tr,ou=personel,ou=people,dc=comu,dc=edu,dc=tr"])
-                attrs['mail'] = "".join([email,"@comu.edu.tr"])
+                email_prefix = mail_adr[0]
+                if email.find(settings.STUDENT_DOMAIN) == -1:
+                    dn="".join(["mail=",email_prefix,"".join(["@", settings.EDUROAM_DOMAIN]),"ou=personel,ou=people,dc=comu,dc=edu,dc=tr"])
+                    attrs['mail'] = "".join([email_prefix,"".join(["@", settings.EDUROAM_DOMAIN])])
+                else:
+                    dn="".join(["mail=",email_prefix,"".join(["@", settings.STUDENT_DOMAIN]),"ou=ogrenci,ou=people,dc=comu,dc=edu,dc=tr"])
+                    attrs['mail'] = "".join([email_prefix,"".join(["@", settings.STUDENT_DOMAIN])])
         else:
-            mail_adr = email.split("@")
-            email = mail_adr[0]
-            dn="".join(["mail=",email,"@comu.edu.tr,ou=personel,ou=people,dc=comu,dc=edu,dc=tr"])
-            attrs['mail'] = "".join([email,"@comu.edu.tr"])
+            email = email
+            dn = "".join(["mail=",email,",ou=personel,ou=people,dc=comu,dc=edu,dc=tr"])
+            attrs['mail'] = email
         if settings.USE_CENTRAL_SERVER:
             attrs['objectclass'] = ['organizationalPerson','radiusprofile','person','inetOrgPerson']
         else:
@@ -62,6 +71,7 @@ class LdapHandler:
             return False
 
     def del_user(self, mail, member_type="personel"):
+         s = ""
          if member_type == "":
              s = "".join(["mail=",mail,",","ou=people,dc=comu,dc=edu,dc=tr"])
          if member_type == "personel" or member_type == "ogrenci":
@@ -75,19 +85,19 @@ class LdapHandler:
 
     def search(self, email): # sadece mail adresine gore ldapta arama yapmak icin
         base_dn = "ou=people,dc=comu,dc=edu,dc=tr"
-        if email.find("@gmail.com") != -1:
+        if email.find(settings.EDUROAM_EXCEPTION_DOMAIN) != -1:
             mail_username = email.split("@")[0]
-            email = "".join([mail_username,"@comu.edu.tr"])
+            email = "".join([mail_username,"".join(["@", settings.EDUROAM_DOMAIN])])
         filter = "".join(['mail=',email])
         self.results = self.server.search_s(base_dn, ldap.SCOPE_SUBTREE, filter) # tek elemanli bir list
         return len(self.results)
 
     def get_cn(self,email): # sadece mail adresine gore kisinin adini soyadini getirir
         base_dn = "ou=people,dc=comu,dc=edu,dc=tr"
-        if email.find("@gmail.com") != -1:
+        if email.find("".join(["@", settings.EDUROAM_EXCEPTION_DOMAIN])) != -1:
             mail_adr = email.split("@")
             email = mail_adr[0]
-            email = "".join([email,"@comu.edu.tr"])
+            email = "".join([email,"".join(["@", settings.EDUROAM_DOMAIN])])
         filter = "".join(['mail=',email])
         attr = ['cn']
         results = self.server.search_s(base_dn,ldap.SCOPE_SUBTREE,filter,attr) # tek elemanli bir list
@@ -96,25 +106,27 @@ class LdapHandler:
         return cn
 
     def modify(self,password,email):
+        personel_email = ""
         self.mod_atr = [( ldap.MOD_REPLACE, 'userPassword', password )]
-        if email.find("@gmail.com") != -1:
+        if email.find("".join(["@", settings.EDUROAM_EXCEPTION_DOMAIN])) != -1:
             mail_adr = email.split("@")
-            email = mail_adr[0]
-            email = "".join([email,"@comu.edu.tr"])
+            email_prefix = mail_adr[0]
+            personel_email = "".join([email_prefix,"".join(["@", settings.EDUROAM_DOMAIN])])
         try:
-            self.server.modify_s("".join(['mail=',email,',ou=personel,ou=people,dc=comu,dc=edu,dc=tr']),self.mod_atr)
+            self.server.modify_s("".join(['mail=',personel_email,',ou=personel,ou=people,dc=comu,dc=edu,dc=tr']),self.mod_atr)
             return True
         except:
             return False
 
     def modify_student(self,password,email):
+        stu_email = ""
         self.mod_atr = [( ldap.MOD_REPLACE, 'userPassword', password )]
-        if email.find("@gmail.com") != -1:
+        if email.find("".join(["@", settings.EDUROAM_EXCEPTION_DOMAIN])) != -1:
             mail_adr = email.split("@")
-            email = mail_adr[0]
-            email = "".join([email,"@comu.edu.tr"])
+            email_prefix = mail_adr[0]
+            stu_email = "".join([email_prefix,"".join(["@", settings.STUDENT_DOMAIN])])
         try:
-            self.server.modify_s("".join(['mail=',email,',ou=ogrenci,ou=people,dc=comu,dc=edu,dc=tr']),self.mod_atr)
+            self.server.modify_s("".join(['mail=',stu_email,',ou=ogrenci,ou=people,dc=comu,dc=edu,dc=tr']),self.mod_atr)
             return True
         except:
             return False
