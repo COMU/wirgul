b0475211cdc61d61592870d9d99dd910828a5c82
ebruAkagunduz <ebru.akagunduz@gmail.com>
2012-10-04 20:16:18 +0300

    dil desteği için stringlere parantezler eklendi

--- utils/messages.py
+++ utils/messages.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 #-*- coding: UTF-8 -*-
+from django.utils.translation import ugettext_lazy as _
 
-REQUIRED_FORM_MESSAGE = "Bu alanı doldurmak zorundasınız"
-INVALID_EMAIL_FORM_MESSAGE = "Geçersiz bir eposta adresi girdiniz"
-INVALID_CAPTCHA_FORM_MESSAGE = "Gördüğünüz harfleri yanlış girdiniz"
-INVALID_DOMAIN_EMAIL = "Alan adınız yanlış"
-INVALID_GUEST_EMAIL = "Yalnızca bir üniversiteye ait e-postalar ile kayıt yapılabilmektedir."
+REQUIRED_FORM_MESSAGE = _(u"Bu alanı doldurmak zorundasınız")
+INVALID_EMAIL_FORM_MESSAGE = _(u"Geçersiz bir eposta adresi girdiniz")
+INVALID_CAPTCHA_FORM_MESSAGE = _(u"Gördüğünüz harfleri yanlış girdiniz")
+INVALID_DOMAIN_EMAIL = _(u"Alan adınız yanlış")
+INVALID_GUEST_EMAIL = _(u"Yalnızca bir üniversiteye ait e-postalar ile kayıt yapılabilmektedir.")
 
-WEB_MODEL_YOURNAME = "İsminiz"
-WEB_MODEL_MIDDLE_NAME = "Orta İsminiz"
-WEB_MODEL_SURNAME = "Soyisminiz"
-WEB_MODEL_EMAIL = "Eposta adresiniz"
-WEB_MODEL_FACULTY = "Fakülteniz"
-WEB_MODEL_DEPARTMENT = "Bölümünüz"
-WEB_MODEL_GUEST_EMAIL = "Misafir olduğunuz kişinin e-posta adresi"
-WEB_MODEL_CHOOSE_DURATION_TYPE = "Saat, gün ya da hafta seçiniz"
-WEB_MODEL_CHOOSE_DURATION = "Kablosuz erişimi kullanacağınız süreyi seçiniz"
-WEB_MODEL_TEL_NUMBER = "Telefon numaranız"
+WEB_MODEL_YOURNAME = _(u"İsminiz")
+WEB_MODEL_MIDDLE_NAME = _(u"Orta İsminiz")
+WEB_MODEL_SURNAME = _(u"Soyisminiz")
+WEB_MODEL_EMAIL = _(u"Eposta adresiniz")
+WEB_MODEL_FACULTY = _(u"Fakülteniz")
+WEB_MODEL_DEPARTMENT = _(u"Bölümünüz")
+WEB_MODEL_GUEST_EMAIL = _(u"Misafir olduğunuz kişinin e-posta adresi")
+WEB_MODEL_CHOOSE_DURATION_TYPE = _(u"Saat, gün ya da hafta seçiniz")
+WEB_MODEL_CHOOSE_DURATION = _(u"Kablosuz erişimi kullanacağınız süreyi seçiniz")
+WEB_MODEL_TEL_NUMBER = _(u"Telefon numaranız")
